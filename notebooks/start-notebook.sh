5273925860456b9dca49847d633ada90e6748252
Kenta Murata <mrkn@mrkn.jp>
2018-04-29 05:45:39 +0800

    Updated for RubyKaigi 2018

--- notebooks/start-notebook.sh
+++ notebooks/start-notebook.sh
@@ -6,4 +6,8 @@ if [[ -n "$JUPYTER_NOTEBOOK_TOKEN" ]]; then
   token="--NotebookApp.token=$JUPYTER_NOTEBOOK_TOKEN"
 fi
 
-. /usr/local/bin/start.sh jupyter notebook $token $*
+if [[ -n "$JUPYTER_ENABLE_LAB" ]]; then
+  . /usr/local/bin/start.sh jupyter lab $token $*
+else
+  . /usr/local/bin/start.sh jupyter notebook $token $*
+fi
